17814ebc92bc42b884a3690f7083b390a5e427aa
Bengt Weiße <bengtler@gmail.com>
2016-01-11 17:56:56 +0100

    add

--- routing-final/app/pipes/pizzaSearch.pipe.js
+++ routing-final/app/pipes/pizzaSearch.pipe.js
@@ -1,5 +1,4 @@
 System.register(['angular2/core'], function(exports_1) {
-    "use strict";
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -37,7 +36,7 @@ System.register(['angular2/core'], function(exports_1) {
                     __metadata('design:paramtypes', [])
                 ], PizzaSearchPipe);
                 return PizzaSearchPipe;
-            }());
+            })();
             exports_1("PizzaSearchPipe", PizzaSearchPipe);
         }
     }
