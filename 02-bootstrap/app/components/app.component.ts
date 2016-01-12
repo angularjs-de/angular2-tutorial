0376cccb846f6c20b2823c5fbc6a6b9d005db6d7
Bengt Weiße <bengtler@gmail.com>
2016-01-12 20:36:09 +0100

    restructuring and adding first steps :)

---
+++ 02-bootstrap/app/components/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'pizza-app',
+    template: `
+    <h1>Willkommen zum Angular2 Tutorial von AngularJS.DE</h1>
+    `
+})
+export class PizzaAppComponent {
+}
